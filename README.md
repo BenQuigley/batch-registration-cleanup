bcc7f0ed396f8ba65208376c31a158bdc124049d
Ben Quigley <benquigleyconsulting@gmail.com>
2017-05-25 08:14:27 -0400

    fixed readme

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-#Batch Registration Cleanup Report
+# Batch Registration Cleanup Report
 
 A suite of scripts to be used as Informer scripted columns for a 
 batch registration cleanup report for Berklee College of Music.
 
-##Recent Changes
+## Recent Changes
 
 The Elective Check field was merged into the Min Credits Check field 
 to avoid redundancy, since an elective is what is assigned to students 
--- lhumplace.js
+++ lhumplace.js
@@ -13,6 +13,8 @@ switch (lhumScore)
 {
     case '':
         break;
+    case 0:
+        break;
     case 1:
         lhumSection = 'LHUM-100-1A';
         break;
@@ -20,154 +22,19 @@ switch (lhumScore)
         lhumSection = 'LHUM-100-1B';
         break;
     case 3:
-        lhumSection = 'LHUM-100-1C';
-        break;
-    case 4:
-        lhumSection = 'LHUM-100-1D';
-        break;
-    case 5:
-        lhumSection = 'LHUM-100-1E';
-        break;
-    case 6:
-        lhumSection = 'LHUM-100-1F';
-        break;
-    case 7:
-        lhumSection = 'LHUM-100-1G';
-        break;
-    case 8:
-        lhumSection = 'LHUM-100-1H';
-        break;
-    case 9:
         lhumSection = 'LHUM-100-2A';
         break;
-    case 10:
+    case 4:
         lhumSection = 'LHUM-100-2B';
         break;
-    case 12:
-        lhumSection = 'LHUM-100-2D';
-        break;
-    case 13:
-        lhumSection = 'LHUM-100-2E';
-        break;
-    case 14:
-        lhumSection = 'LHUM-100-2F';
-        break;
-    case 15:
-        lhumSection = 'LHUM-100-2G';
-        break;
-    case 16:
-        lhumSection = 'LHUM-100-2H';
-        break;
-    case 17:
-        lhumSection = 'LHUM-100-2I';
-        break;
-    case 18:
-        lhumSection = 'LHUM-100-2J';
-        break;
-    case 19:
+    case 5:
         lhumSection = 'LHUM-100-3A';
         break;
-    case 20:
+    case 6:
         lhumSection = 'LHUM-100-3B';
         break;
-    case 21:
-        lhumSection = 'LHUM-100-3C';
-        break;
-    case 22:
-        lhumSection = 'LHUM-100-3D';
-        break;
-    case 23:
-        lhumSection = 'LHUM-100-3E';
-        break;
-    case 24:
-        lhumSection = 'LHUM-100-3F';
-        break;
-    case 25:
-        lhumSection = 'LHUM-100-3G';
-        break;
-    case 26:
-        lhumSection = 'LHUM-100-3H';
-        break;
-    case 27:
-        lhumSection = 'LHUM-100-4A';
-        break;
-    case 28:
-        lhumSection = 'LHUM-100-4B';
-        break;
-    case 29:
-        lhumSection = 'LHUM-100-4C';
-        break;
-    case 30:
-        lhumSection = 'LHUM-100-4D';
-        break;
-    case 31:
-        lhumSection = 'LHUM-100-4E';
-        break;
-    case 32:
-        lhumSection = 'LHUM-100-4F';
-        break;
-    case 33:
-        lhumSection = 'LHUM-100-4G';
-        break;
-    case 34:
-        lhumSection = 'LHUM-100-4H';
-        break;
-    case 35:
-        lhumSection = 'LHUM-100-9C';
-        break;
-    case 36:
-        lhumSection = 'LHUM-100-5A';
-        break;
-    case 37:
-        lhumSection = 'LHUM-100-5B';
-        break;
-    case 38:
-        lhumSection = 'LHUM-100-5C';
-        break;
-    case 39:
-        lhumSection = 'LHUM-100-5D';
-        break;
-    case 40:
-        lhumSection = 'LHUM-100-5E';
-        break;
-    case 41:
-        lhumSection = 'LHUM-100-5F';
-        break;
-    case 42:
-        lhumSection = 'LHUM-100-5G';
-        break;
-    case 43:
-        lhumSection = 'LHUM-100-5H';
-        break;
-    case 45:
-        lhumSection = 'LHUM-100-6A';
-        break;
-    case 46:
-        lhumSection = 'LHUM-100-6B';
-        break;
-    case 47:
-        lhumSection = 'LHUM-100-6C';
-        break;
-    case 48:
-        lhumSection = 'LHUM-100-6D';
-        break;
-    case 49:
-        lhumSection = 'LHUM-100-6E';
-        break;
-    case 50:
-        lhumSection = 'LHUM-100-6F';
-        break;
-    case 51:
-        lhumSection = 'LHUM-100-6G';
-        break;
-    case 53:
+    case 7:
         lhumSection = 'LHUM-100-9A';
-        break;
-    case 54:
-        lhumSection = 'LHUM-100-9B';
-        break;
-    case 55:
-        lhumSection = 'LHUM-100-1I';
         break;
 }
 if (lhumSection != '')

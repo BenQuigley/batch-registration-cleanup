712e3d1383961a546a3675799670f241bc7acb45
Ben Quigley <benquigleyconsulting@gmail.com>
2017-07-17 15:03:37 -0400

    removed old code

--- transfercreditcheck.js
+++ transfercreditcheck.js
@@ -9,20 +9,3 @@ for (i in coursenumberactive)
 }
 
 duplicateCourses ? "<div style='color:#990000'>The student is enrolled for the following courses which their transfer credits make redundant:\n"+duplicateCourses.join() : "<div style='color:navy'>No transfer credit redundancies found.";
-
-
-
-
-
-
-
-var completedcourses = transfercourses.concat(bSttrNcCourses).sort().join();
-var duplicateCourses = [];
-for (i in coursenumberactive) 
-{
-    if (completedcourses.indexOf(coursenumberactive[i]) >-1) 
-    {
-        duplicateCourses.push(coursenumberactive[i]);
-    }
-}
-duplicateCourses.length === 0 ? "<div style='color:navy'>No transfer credit redundancies found." : "<div style='color:#990000'>The student is enrolled for the following courses which their transfer credits make redundant:\n"+duplicateCourses.join();

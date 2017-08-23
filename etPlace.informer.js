5676a8ff345255e8df9fcbc9fe67fe3d82de7e7a
Ben Quigley <benquigleyconsulting@gmail.com>
2017-08-23 13:56:53 -0400

    refactor

--- etPlace.informer.js
+++ etPlace.informer.js
@@ -1,63 +1,35 @@
 var etClass = "";
 var academic = scoreaiacademic;
-var rawWriting = bSttrRawWsScore;gi
-switch(scoreet)
+var rawWriting = bSttrRawWsScore;
+if (scoreet === null) {
+    etClass = "XX-500";
+}
+else if (0 <= scoreet <= 13 ) {
+    if (academic === 0 && 0 <= rawWriting <= 40) {
+        etClass = "ET-110";
+    }
+    else if (scoreet <= 6 && academic === 1 && 0 <= rawWriting <= 40) {
+        etClass = "ET-110";
+    }
+    else if (scoreet <= 6 && academic === 2 && 0 <= rawWriting <= 20) {
+        etClass = "ET-110";
+    }
+    else if (7 <= scoreet <= 13 && academic === 1 && 0 <= rawWriting <= 20) {
+        etClass = "ET-110";
+    }
+    else {
+        etClass = "ET-111";
+    }
+}
+else if (scoreet <= 16) {
+    etClass = "ET-112";
+}
+else if (scoreet <= 19) {
+    etClass = "ET-211";
+}
+else if (scoreet <= 22)
 {
-    case null:
-        etClass = "XX-500";
-        break;
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-        if (academic === 1 && 0 <= rawWriting <= 40)
-        {
-            etClass = "ET-110";
-            break;
-        }
-        else if (academic === 2 && 0 <= rawWriting <= 20)
-        {
-            etClass = "ET-110";
-            break;
-        }
-    case 7:
-    case 8:
-    case 9:
-    case 10:
-    case 11:
-    case 12:
-    case 13:
-        if (academic === 0 && 0 <= rawWriting <= 40)
-        {
-            etClass = "ET-110";
-            break;
-        }
-        else if (academic === 1 && 0 <= rawWriting <= 20)
-        {
-            etClass = "ET-110";
-            break;
-        }
-        else
-        {
-            etClass = "ET-111";
-            break;
-        }
-    case 14:
-    case 15:
-    case 16:
-        etClass = "ET-112";
-        break;
-    case 17:
-    case 18:
-    case 19:
-        etClass = "ET-211";
-        break;
-    case 20:
-    case 21:
-    case 22:
-        etClass = "ET-212";
+    etClass = "ET-212";
 }
 if (coursenumberactive.join().search(etClass)>-1)
 {

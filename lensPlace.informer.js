34b9c8907dc71c07d104d678fee6753b1fee3096
Ben Quigley <benquigleyconsulting@gmail.com>
2017-08-23 13:22:31 -0400

    updated lens placement for 2017FA

--- lensPlace.informer.js
+++ lensPlace.informer.js
@@ -1,31 +1,272 @@
-var lensScore = parseInt(bSttrLhumScore);
+var lensScore = Number(bSttrLhumScore);
 var lensSection = new String(); 
 switch (lensScore)
 {
+
     case '':
         break;
     case 0:
         break;
     case 1:
-        lensSection = 'LHUM-100-1A';
+        lensSection = 'LENS-P101-1A';
         break;
     case 2:
-        lensSection = 'LHUM-100-1B';
+        lensSection = 'LENS-P101-1B';
         break;
     case 3:
-        lensSection = 'LHUM-100-2A';
+        lensSection = 'LENS-P101-1C';
         break;
     case 4:
-        lensSection = 'LHUM-100-2B';
+        lensSection = 'LENS-P101-1D';
         break;
     case 5:
-        lensSection = 'LHUM-100-3A';
+        lensSection = 'LENS-P101-1E';
         break;
     case 6:
-        lensSection = 'LHUM-100-3B';
+        lensSection = 'LENS-P101-2A';
         break;
     case 7:
-        lensSection = 'LHUM-100-9A';
+        lensSection = 'LENS-P101-2B';
+        break;
+    case 8:
+        lensSection = 'LENS-P101-2C';
+        break;
+    case 9:
+        lensSection = 'LENS-P101-3A';
+        break;
+    case 10:
+        lensSection = 'LENS-P101-3B';
+        break;
+    case 11:
+        lensSection = 'LENS-P101-3C';
+        break;
+    case 12:
+        lensSection = 'LENS-P101-3D';
+        break;
+    case 13:
+        lensSection = 'LENS-P101-4A';
+        break;
+    case 14:
+        lensSection = 'LENS-P101-4B';
+        break;
+    case 15:
+        lensSection = 'LENS-P101-4C';
+        break;
+    case 16:
+        lensSection = 'LENS-P101-4D';
+        break;
+    case 17:
+        lensSection = 'LENS-P101-5A';
+        break;
+    case 18:
+        lensSection = 'LENS-P101-5B';
+        break;
+    case 19:
+        lensSection = 'LENS-P101-5C';
+        break;
+    case 20:
+        lensSection = 'LENS-P101-5D';
+        break;
+    case 21:
+        lensSection = 'LENS-P101-6A';
+        break;
+    case 22:
+        lensSection = 'LENS-P101-6B';
+        break;
+    case 23:
+        lensSection = 'LENS-P101-6C';
+        break;
+    case 24:
+        lensSection = 'LENS-P101-6D';
+        break;
+    case 25:
+        lensSection = 'LENS-P103-1A';
+        break;
+    case 26:
+        lensSection = 'LENS-P103-1B';
+        break;
+    case 27:
+        lensSection = 'LENS-P103-1C';
+        break;
+    case 28:
+        lensSection = 'LENS-P103-2A';
+        break;
+    case 29:
+        lensSection = 'LENS-P103-2B';
+        break;
+    case 30:
+        lensSection = 'LENS-P103-2C';
+        break;
+    case 31:
+        lensSection = 'LENS-P103-3A';
+        break;
+    case 32:
+        lensSection = 'LENS-P103-3B';
+        break;
+    case 33:
+        lensSection = 'LENS-P103-3C';
+        break;
+    case 34:
+        lensSection = 'LENS-P103-3D';
+        break;
+    case 35:
+        lensSection = 'LENS-P103-4A';
+        break;
+    case 36:
+        lensSection = 'LENS-P103-4B';
+        break;
+    case 37:
+        lensSection = 'LENS-P103-4C';
+        break;
+    case 38:
+        lensSection = 'LENS-P103-5A';
+        break;
+    case 39:
+        lensSection = 'LENS-P103-5B';
+        break;
+    case 40:
+        lensSection = 'LENS-P103-6A';
+        break;
+    case 41:
+        lensSection = 'LENS-P103-6B';
+        break;
+    case 42:
+        lensSection = 'LENS-P103-6C';
+        break;
+    case 43:
+        lensSection = 'LENS-P103-9A';
+        break;
+    case 44:
+        lensSection = 'LENS-P105-1A';
+        break;
+    case 45:
+        lensSection = 'LENS-P105-1C';
+        break;
+    case 46:
+        lensSection = 'LENS-P105-1D';
+        break;
+    case 47:
+        lensSection = 'LENS-P105-2A';
+        break;
+    case 48:
+        lensSection = 'LENS-P105-2B';
+        break;
+    case 49:
+        lensSection = 'LENS-P105-2C';
+        break;
+    case 50:
+        lensSection = 'LENS-P105-3A';
+        break;
+    case 51:
+        lensSection = 'LENS-P105-3B';
+        break;
+    case 52:
+        lensSection = 'LENS-P105-4A';
+        break;
+    case 53:
+        lensSection = 'LENS-P105-4B';
+        break;
+    case 54:
+        lensSection = 'LENS-P105-4C';
+        break;
+    case 55:
+        lensSection = 'LENS-P105-4D';
+        break;
+    case 56:
+        lensSection = 'LENS-P105-5A';
+        break;
+    case 57:
+        lensSection = 'LENS-P105-5B';
+        break;
+    case 58:
+        lensSection = 'LENS-P105-6B';
+        break;
+    case 59:
+        lensSection = 'LENS-P105-6C';
+        break;
+    case 60:
+        lensSection = 'LENS-P105-6D';
+        break;
+    case 61:
+        lensSection = 'LENS-P107-1A';
+        break;
+    case 62:
+        lensSection = 'LENS-P107-9A';
+        break;
+    case 63:
+        lensSection = 'LENS-P107-9B';
+        break;
+    case 64:
+        lensSection = 'LENS-P101-C1C';
+        break;
+    case 65:
+        lensSection = 'LENS-P101-C1D';
+        break;
+    case 66:
+        lensSection = 'LENS-P101-C2A';
+        break;
+    case 67:
+        lensSection = 'LENS-P101-C2D';
+        break;
+    case 68:
+        lensSection = 'LENS-P103-C2D';
+        break;
+    case 69:
+        lensSection = 'LENS-P103-C2E';
+        break;
+    case 70:
+        lensSection = 'LENS-P103-C4B';
+        break;
+    case 71:
+        lensSection = 'LENS-P103-C4D';
+        break;
+    case 72:
+        lensSection = 'LENS-P103-C5C';
+        break;
+    case 73:
+        lensSection = 'LENS-P103-C5E';
+        break;
+    case 74:
+        lensSection = 'LENS-P103-C6A';
+        break;
+    case 75:
+        lensSection = 'LENS-P105-C1B';
+        break;
+    case 76:
+        lensSection = 'LENS-P105-C1E';
+        break;
+    case 77:
+        lensSection = 'LENS-P105-C2D';
+        break;
+    case 78:
+        lensSection = 'LENS-P105-C3B';
+        break;
+    case 79:
+        lensSection = 'LENS-P105-C3C';
+        break;
+    case 80:
+        lensSection = 'LENS-P105-C3D';
+        break;
+    case 81:
+        lensSection = 'LENS-P105-C3E';
+        break;
+    case 82:
+        lensSection = 'LENS-P105-C4A';
+        break;
+    case 83:
+        lensSection = 'LENS-P105-C5A';
+        break;
+    case 84:
+        lensSection = 'LENS-P105-C5C';
+        break;
+    case 85:
+        lensSection = 'LENS-P105-C5D';
+        break;
+    case 86:
+        lensSection = 'LENS-P107-C3A';
+        break;
+    case 87:
+        lensSection = 'LENS-P107-C3B';
         break;
 }
 if (lensSection != '')

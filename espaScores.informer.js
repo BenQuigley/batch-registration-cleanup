9ddd0dac182b3b661806760eb02faa69c3b61741
Ben Quigley <benquigleyconsulting@gmail.com>
2017-08-24 11:50:17 -0400

    text update

--- espaScores.informer.js
+++ espaScores.informer.js
@@ -1,4 +1,4 @@
-var espas = "ET: "+scoreet+"\nHR: "+scorehr+"\nPW: "+scorepw+"\nESL: "+scoremichigan+"\nLHUM code: "+
+var espas = "ET: "+scoreet+"\nHR: "+scorehr+"\nPW: "+scorepw+"\nESL: "+scoremichigan+"\nLENS: "+
     bSttrLhumScore+"\nAcad: "+scoreaiacademic+"\nWriting: "+bSttrRawWsScore;
 espas = espas.replace(/null/g,"none")
 espas;

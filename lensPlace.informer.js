01140c4be93690b2e14bc1f0d33940d9150e7369
Ben Quigley <benquigleyconsulting@gmail.com>
2017-08-30 11:22:35 -0400

    update attempt

--- lensPlace.informer.js
+++ lensPlace.informer.js
@@ -2,8 +2,10 @@ var lensScore = Number(bSttrLhumScore);
 var lensSection = '';
 switch (lensScore) {
     case '':
+        lensSection = 'unassigned';
         break;
     case 0:
+        lensSection = 'none';
         break;
     case 1:
         lensSection = 'LENS-P101-1A';
@@ -267,22 +269,16 @@ switch (lensScore) {
         lensSection = 'LENS-P107-C3B';
         break;
 }
-if (lensSection != '') {
-    var section_match = "";
-    for (i in coursenumberandsectionactive) {
-        var sec = coursenumberandsectionactive[i];
-        if (sec.indexOf(lensSection) >= 0) {
-            section_match = sec;
-            break;
-        }
-    }
-    if (section_match) {
-        "<div style='color:navy'>"+section_match+": done."
-    } 
-    else {
-        "<div style='color:#990000'>"+lensSection+": needed."
-    }
+var section_match = coursenumberandsectionactive.indexOf(lensSection) >= 0;
+if (lensSection === 'unassigned') {
+    "<div style='color:#990000'>Assignment needed: contact Liberal Arts chair.";
+}
+else if (lensSection === 'none') {
+    "<div style='color:navy'> None assigned.";
+}
+else if (section_match) {
+    "<div style='color:navy'>"+section_match+": done.";
 }
 else {
-    "<div style='color:navy'> None assigned."
+    "<div style='color:#990000'>"+lensSection+": needed.";
 }

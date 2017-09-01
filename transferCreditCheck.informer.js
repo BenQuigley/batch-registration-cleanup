db8fc5085c9040a08e54132cb4af67cbeb487c49
Ben Quigley <benquigleyconsulting@gmail.com>
2017-09-01 13:46:17 -0400

    removed unnecessary sort

--- transferCreditCheck.informer.js
+++ transferCreditCheck.informer.js
@@ -4,18 +4,17 @@
  any currently enrolled courses that are redundant.
  */
 
-var completedCourses = transfercourses.concat(bSttrNcCourses).sort();
+var completedCourses = transfercourses.concat(bSttrNcCourses);
 var duplicateCourses = [];
 
 // For each LAHS (deprecated history code) credit, also consider
 // its LVIS (new code) equivalent.
-// For each LENG-201 credit, also consider its new code, LENG-223.
-for (i in completedCourses) {
+for (var i in completedCourses) {
     var completedCourse = completedCourses[i];
     if (completedCourse.indexOf("LAHS") === 0) {
         completedCourses.push(completedCourse.replace("LAHS", "LVIS"));
     }
-    else if (completedCourse === 'LENG-201') {
+    else if (completedCourse == 'LENG-201') {
         completedCourses.push('LENG-223');
     }
 }
@@ -23,11 +22,16 @@ for (i in completedCourses) {
 // List all currently enrolled courses that are redundant
 // because of transfer or noncourse credit.
 var completedCoursesString = completedCourses.join();
-for (j in coursenumberactive) {
+for (var j in coursenumberactive) {
     var course = coursenumberactive[j];
     if (completedCoursesString.indexOf(course) >-1)
     {
         duplicateCourses.push(course);
     }
 }
-duplicateCourses ? "<div style='color:#990000'>The student is enrolled for the following courses which their transfer credits make redundant:\n"+duplicateCourses.join() +"</div>" : "<div style='color:navy'>No transfer credit redundancies found.</div>";
+if (duplicateCourses.length > 0) {
+    "<div style='color:navy'>No transfer credit redundancies found.</div>"
+}
+else {
+    "<div style='color:#990000'>The student is enrolled for the following courses which their transfer credits make redundant:\n" + duplicateCourses.join() + "</div>";
+}
